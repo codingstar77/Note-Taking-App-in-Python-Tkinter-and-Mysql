129e8085ead933161e34803dfc9df6815a67c161
Kaustubh Devkar <kdevkar1998@gmail.com>
2018-01-08 06:52:45 +0530

    Added screenshot

--- start.py
+++ start.py
@@ -4,7 +4,7 @@ from dashboard import Dashboard
 if __name__=="__main__":
     try:
         #Add Your Database username and password here
-        db=NoteDB(username="root",password="root")
+        db=NoteDB(username="root",password="kd99")
         Dashboard().initUI(db)
     except Exception as e:
         messagebox.showinfo("Error","Unable to establish database connection.")
